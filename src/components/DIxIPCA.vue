e79a733af3aff5265ebf0c85f522d15f5bf6c169
Gilmar Andrade <gilmar-andrade@outlook.com>
2023-02-16 19:09:16 -0300

    chart js

--- src/components/DIxIPCA.vue
+++ src/components/DIxIPCA.vue
@@ -1,13 +1,66 @@
 <template>
-  <h1>DI x IPCA</h1>
-  <pre>
-    {{ipca}}
-  </pre>
+  <h1>CDI x IPCA</h1>
+  <Line :data="data" :options="options" />
+
+  <div style="display: flex">
+    <pre>
+      <h2>CDI</h2>
+      {{cdi}}
+    </pre>
+    <pre>
+      <h2>IPCA</h2>
+      {{ipca}}
+    </pre>
+  </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+const data = ref({
+    labels: ['01/23', '02/23', '03/23', '04/23', '05/23', '06/23', '07/23'],
+    datasets: [
+      {
+        label: 'CDI',
+        backgroundColor: 'blue',
+        data: [40, 39, 10, 40, 39, 80, 40]
+      },
+      {
+        label: 'IPCA',
+        backgroundColor: 'red',
+        data: [20, 10, 40, 20, 45, 56, 78]
+      }
+    ]
+})
+  
+const options = ref({
+    // responsive: true,
+    // maintainAspectRatio: false
+})
+  
 
 const cdi = ref([])
 const ipca = ref([])
